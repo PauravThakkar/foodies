22d58086124dd3e3cea7d7cc93aecb19d23164de
Gargi Purandare <135798162+gargirp@users.noreply.github.com>
2024-03-09 16:48:41 -0700

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 
 class UserSetting(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    recent_restaurant = models.CharField(max_length=50, blank=True, null=True)
     favorite_cuisine = models.CharField(max_length=50, blank=True, null=True)
     notifications_enabled = models.BooleanField(default=True)
     email_frequency = models.CharField(max_length=20, choices=[('daily', 'Daily'), ('weekly', 'Weekly')], default='daily')
