ee0c0cae25835c326f41717f0f6790a3f1e388af
Astha <135056448+ABUoW@users.noreply.github.com>
2024-03-21 15:54:36 -0400

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    # Add more fields as needed
+
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=100)
@@ -16,12 +16,6 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     cuisines = models.ManyToManyField(Cuisine)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=20)
-    website = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
