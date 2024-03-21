975f5f45155964df9e9e4c68f2b5ea8af0d60d95
Astha <73028141+Astha-20-PY@users.noreply.github.com>
2024-03-21 16:09:42 -0400

    Changed

--- app/models.py
+++ app/models.py
@@ -97,3 +97,38 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review for {self.restaurant} by {self.user.username} ({self.ratings} stars)"
+
+
+
+#Astha
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Admin(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
