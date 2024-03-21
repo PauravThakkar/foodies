568f25a4e60babc43b77c57afdfebf713bfe92bf
Astha <135056448+ABUoW@users.noreply.github.com>
2024-03-21 16:01:05 -0400

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from .models import Restaurant
+from django.shortcuts import render, redirect
+from .models import Restaurant, MenuItem, Admin
+from .forms import MenuItemForm
+
 
 def restaurant_list(request):
     restaurants = Restaurant.objects.all()
     return render(request, 'restaurant_list.html', {'restaurants': restaurants})
 
-def restaurant_detail(request, restaurant_id):
-    restaurant = Restaurant.objects.get(id=restaurant_id)
-    return render(request, 'restaurant_detail.html', {'restaurant': restaurant})
+
+def menu_items(request, restaurant_id):
+    restaurant = Restaurant.objects.get(pk=restaurant_id)
+    menu_items = MenuItem.objects.filter(restaurant=restaurant)
+    return render(request, 'menu_items.html', {'restaurant': restaurant, 'menu_items': menu_items})
+
+
+def add_menu_item(request):
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurant_list')
+    else:
+        form = MenuItemForm()
+    return render(request, 'add_menu_item.html', {'form': form})
